47340cddd3b190c3f2dfadf54f4632a8c8052901
Geovani Lopes Sampaio <gls.eng20@uea.edu.br>
2023-07-12 20:31:57 -0400

    Adicionar arquivos de projeto.

---
+++ StockPriceAlert/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace StockPriceAlert
+{
+    class EmailSender
+    {
+        public static void SendEmail(string remetente, string destinatario, string result)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Tester Remetente", remetente));
+            message.To.Add(new MailboxAddress("Tester Destinatario", destinatario));
+            message.Subject = "Atenção!";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Olá, isso é um teste3. " + result
+            };
+
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+
+                client.Authenticate("stockquotealert1@gmail.com", "tyfqlotifdoulizp");
+
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro!");
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+            client.Disconnect(true);
+
+        }
+    }
+}
---
+++ StockPriceAlert/Program.cs
@@ -0,0 +1,33 @@
+﻿using StockPriceAlert;
+
+class Program
+{
+   public static void Main(string[] args)
+    {
+
+        // Aguarda 5 segundos entre cada chama do metodo de envio do email
+        Timer timer = new Timer(CheckQuotePrice, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+        Console.ReadLine();
+    }
+
+    private static void CheckQuotePrice(object state)
+    {
+        // Simula um valor de cotação.
+        Random random = new Random();
+        decimal stockQuote = random.Next(5, 25);
+        Console.WriteLine(stockQuote);
+
+        // Verifica se a cotação atinge algum limite.
+        if (stockQuote <= 10)
+        {
+            // Envia o email
+            EmailSender.SendEmail("stockquotealert1@gmail.com", "geovanilopes2002@gmail.com", "compre!");
+
+        }
+        else if (stockQuote >= 20)
+        {
+            EmailSender.SendEmail("stockquotealert1@gmail.com", "geovanilopes2002@gmail.com", "venda!");
+        }
+    }
+}
