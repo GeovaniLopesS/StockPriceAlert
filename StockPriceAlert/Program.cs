65432f442b90550dd38f788d65fd1ca722a8655c
Geovani Lopes Sampaio <gls.eng20@uea.edu.br>
2023-07-13 00:31:10 -0400

    Atualizacao na obtençao da cotação da ação

--- StockPriceAlert/Program.cs
+++ StockPriceAlert/Program.cs
@@ -15,19 +15,19 @@ class Program
     // Obtem o valor da ação no momento da consulta
     private static void CheckStockQuote(object state)
     {
-       
+       // definindo a url e instanciando um Uri 
         string QUERY_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=PETR4.SA&apikey=88YPN0WHXEHKRW30";
         Uri queryUri = new Uri(QUERY_URL);
 
         using (WebClient client = new WebClient())
         {
+            // realizando requisicao para a url e salvando como string
             string jsonData = client.DownloadString(queryUri);
-
             Console.WriteLine(jsonData);
 
+            // obtendo o valor da ação a partir das chaves "Global Quote" e "05. price"
             JsonDocument jsonDocument = JsonDocument.Parse(jsonData);
             JsonElement jsonElement = jsonDocument.RootElement.GetProperty("Global Quote");
-            
             string stockQuote = jsonElement.GetProperty("05. price").ToString();
 
             Console.WriteLine($"Stock quote: {stockQuote}");
