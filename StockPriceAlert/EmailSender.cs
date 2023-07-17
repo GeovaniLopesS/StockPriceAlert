0ee3a2e2947df91208bbd94f7cfc934e61db8431
Geovani Lopes Sampaio <93932959+GeovaniLopesS@users.noreply.github.com>
2023-07-16 23:52:24 -0400

    otimizacao do programa: ajuste na leitura dos arquivos de configuração do servidor smpt e api

--- StockPriceAlert/EmailSender.cs
+++ StockPriceAlert/EmailSender.cs
@@ -2,41 +2,45 @@
 using System.Text.Json;
 using MailKit.Net.Smtp;
 using MimeKit;
-using static System.Net.Mime.MediaTypeNames;
+
 
 namespace StockPriceAlert
 {
     class EmailSender
-    {
+    {   
+        // credenciais do email que envia a mensagem
+        private static string senderEmail;
+        private static string senderPassword;
+
+        // configuraçoes do servidor smtp 
+        // email que recebe a mensagem
+        private static string reciever;
+        private static string smtpHost;
+        private static int smtpPort;
+        private static bool useSsl;
+
         public static void SendEmail(string body)
         {
-            // lendo arquivo json com as configurações do servidor smtp
-            JsonDocument serverConfig = JsonDocument.Parse(File.ReadAllText("C:\\Users\\geova\\Source\\Repos\\StockPriceAlert\\StockPriceAlert\\serverconfig.json"));
-            JsonElement serverConfigElement = serverConfig.RootElement;
-
-            // recuperando informação das credenciais do email que enviará
-            string senderEmail = Environment.GetEnvironmentVariable("appEmail");
-            string senderPassword = Environment.GetEnvironmentVariable("appPassword");
-
             // define o remetente, destinatario, titulo e corpo do email
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("Tester Remetente", senderEmail));
-            message.To.Add(new MailboxAddress("Tester Destinatario", serverConfigElement.GetProperty("reciever").GetString()));
+            message.To.Add(new MailboxAddress("Tester Destinatario", reciever));
             message.Subject = "Atenção!";
             message.Body = new TextPart("plain")
+
             {
                 Text = body
             };
 
             // instancia um smtp client para enviar o email
             SmtpClient client = new SmtpClient();
-
             try
             {
-                client.Connect(serverConfigElement.GetProperty("host").GetString(), 
-                    serverConfigElement.GetProperty("port").GetInt16(), 
-                    serverConfigElement.GetProperty("useSsl").GetBoolean());
-                client.Authenticate(senderEmail, "tyfqlotifdoulizp");
+                client.Connect(smtpHost, 
+                    smtpPort, 
+                    useSsl);
+
+                client.Authenticate(senderEmail, senderPassword);
                 client.Send(message);
             }
             catch (Exception ex)
@@ -44,6 +48,24 @@ namespace StockPriceAlert
                 Console.WriteLine(ex.ToString());
             }
             client.Disconnect(true);
+        }
+
+        public static void LoadConfig()
+        {
+            // lendo arquivo json com as configurações do servidor smtp
+            string programDiretory = System.IO.Directory.GetCurrentDirectory();
+            JsonDocument serverConfig = JsonDocument.Parse(File.ReadAllText(programDiretory + "\\serverconfig.json"));
+            JsonElement serverConfigElement = serverConfig.RootElement;
+
+            // carrega valores do servidor smtp
+            smtpHost = serverConfigElement.GetProperty("host").GetString();
+            smtpPort = serverConfigElement.GetProperty("port").GetInt16();
+            useSsl = serverConfigElement.GetProperty("useSsl").GetBoolean();
+            reciever = serverConfigElement.GetProperty("reciever").GetString();
+
+            // recuperando informação das credenciais do email que enviará
+            senderEmail = Environment.GetEnvironmentVariable("appEmail");
+            senderPassword = Environment.GetEnvironmentVariable("appPassword");
         }
     }
 }
--- StockPriceAlert/Program.cs
+++ StockPriceAlert/Program.cs
@@ -5,11 +5,14 @@ using System.Text.Json;
 class Program
 {
     // salva o valor da última cotação recuperada
-    private static decimal currentQuote { get; set; }
-
+    private static decimal currentQuote = 0;
+    private static string queryUrl;
+    private static decimal stockPrice;
     public static void Main(string[] args)
     {
-        currentQuote = 0;
+        // configura informaçoes do servidor smtp e api
+        EmailSender.LoadConfig();
+        SetQueryUrl(args[0]);
 
         // chama o metodo da API 
         while (true)
@@ -19,55 +22,63 @@ class Program
         }
     }
 
+    private static void SetQueryUrl(string stock)
+    {
+        // carrega json de configuração da string
+        string programDiretory = Directory.GetCurrentDirectory();
+        JsonElement apiConfigJson = JsonDocument.Parse(File.ReadAllText(programDiretory + "\\apiconfig.json")).RootElement;
+
+        queryUrl = apiConfigJson.GetProperty("url").GetString().Replace("{stock}", stock + ".SA").Replace("{apiKey}", 
+            apiConfigJson.GetProperty("apiKey").GetString());
+        Console.WriteLine(queryUrl);
+    }
+
     // Obtem o valor da ação no momento da consulta
     private static void CheckStockQuote(string stock, decimal upperBound, decimal lowerBound)
-    {  
-        string url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
-        string apiKey = "&apikey=88YPN0WHXEHKRW30";
+    {
 
         // definindo a url e instanciando um Uri 
-        string QUERY_URL = url + stock + ".SA" + apiKey;
-        Uri queryUri = new Uri(QUERY_URL);
+        Uri queryUri = new Uri(queryUrl);
 
         using (WebClient client = new WebClient())
         {
             // realizando requisicao para a url e salvando como string
             string jsonData = client.DownloadString(queryUri);
-            Console.Write(jsonData);
+            Console.Write("json data:" +  jsonData);
 
-            // obtendo o valor da ação a partir da chave"05. price"
-            JsonDocument jsonDocument = JsonDocument.Parse(jsonData);
             try
             {
+                // obtendo o valor da ação a partir da chave"05. price"
+                JsonDocument jsonDocument = JsonDocument.Parse(jsonData);
                 JsonElement jsonElement = jsonDocument.RootElement.GetProperty("Global Quote");
+                string stockPricestring = jsonElement.GetProperty("05. price").GetString();
+                stockPrice = Convert.ToDecimal(stockPrice, CultureInfo.InvariantCulture);
+
             }
             catch (Exception e) 
             {
                 Console.WriteLine(e.Message);   
             }   
 
-            string stockPrice = jsonElement.GetProperty("05. price").GetString();    
-            decimal stockQuote = Convert.ToDecimal(stockPrice, CultureInfo.InvariantCulture);
-
             //verifica se o valor encontrado é diferente do último valor verificado
-            if (stockQuote != currentQuote)
+            if (stockPrice != currentQuote)
             {
                 // verifica se a cotação atingiu algum limite e envia o email
-                if (stockQuote >= upperBound)
+                if (stockPrice >= upperBound)
                 {
-                    string body = "A ação " + stock + " atingiu R$" + stockQuote + ". venda!";
+                    string body = "A ação " + stock + " atingiu R$" + stockPrice + ". venda!";
                     EmailSender.SendEmail(body);
 
                 }
-                else if (stockQuote <= lowerBound)
+                else if (stockPrice <= lowerBound)
                 {
-                    string body = "A ação " + stock + " atingiu R$" + stockQuote + ". compre!";
+                    string body = "A ação " + stock + " atingiu R$" + stockPrice + ". compre!";
                     EmailSender.SendEmail(body);
 
                 }
                 Console.WriteLine("Email enviado!");
                 //salva o valor da cotação para controle
-                currentQuote = stockQuote;
+                currentQuote = stockPrice;
             }
             // se o valor se manteve, então não envia email.
             else
