75bb629a22c7e8a9ac5c35f988f56660d62bb684
Geovani Lopes Sampaio <93932959+GeovaniLopesS@users.noreply.github.com>
2023-07-18 23:18:37 -0400

    criando classe para para comsumir a api

---
+++ StockPriceAlert/ApiFetcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StockPriceAlert
+{
+    static class ApiFetcher
+    {
+        private static string queryUrl;
+        private static string jsonData;
+
+        static ApiFetcher()
+        {
+            SetQueryUrl();
+        }
+        public static string GetStockData(string stock)
+        {
+            // definindo a url e instanciando um Uri 
+            Uri queryUri = new Uri(queryUrl.Replace("{stock}", stock + ".SA"));
+
+            using (WebClient client = new WebClient())
+            {
+                // realizando requisicao para a url e salvando como string
+                jsonData = client.DownloadString(queryUri);  
+            }
+            
+            return jsonData;
+        }
+        // define a url com a api key
+        private static void SetQueryUrl()
+        {
+            // carrega json de configuração da string
+            string programDiretory = Directory.GetCurrentDirectory();
+            JsonElement apiConfigJson = JsonDocument.Parse(File.ReadAllText(programDiretory + "\\apiconfig.json")).RootElement;
+
+            queryUrl = apiConfigJson.GetProperty("url").GetString().Replace("{apiKey}",
+                apiConfigJson.GetProperty("apiKey").GetString());
+        }
+    }
+}
--- StockPriceAlert/Program.cs
+++ StockPriceAlert/Program.cs
@@ -6,90 +6,63 @@ class Program
 {
     // salva o valor da última cotação recuperada
     private static double currentQuote = 0;
-    private static string queryUrl;
-    private static double stockPrice;
+
+
     public static void Main(string[] args)
     {
         // configura informaçoes do servidor smtp e api
         EmailSender.LoadConfig();
-        SetQueryUrl(args[0]);
 
         // chama o metodo da API 
         while (true)
         {
             CheckStockQuote(args[0], double.Parse(args[1]), double.Parse(args[2]));
-
-            Thread.Sleep(12000);
+            
+            Thread.Sleep(5000);
         }
-    }
-
-    private static void SetQueryUrl(string stock)
-    {
-        // carrega json de configuração da string
-        string programDiretory = Directory.GetCurrentDirectory();
-        JsonElement apiConfigJson = JsonDocument.Parse(File.ReadAllText(programDiretory + "\\apiconfig.json")).RootElement;
-
-        queryUrl = apiConfigJson.GetProperty("url").GetString().Replace("{stock}", stock + ".SA").Replace("{apiKey}", 
-            apiConfigJson.GetProperty("apiKey").GetString());
     }
 
     // Obtem o valor da ação no momento da consulta
     private static void CheckStockQuote(string stock, double upperBound, double lowerBound)
     {
+        string jsonData = ApiFetcher.GetStockData(stock);
 
-        // definindo a url e instanciando um Uri 
-        Uri queryUri = new Uri(queryUrl);
-        using (WebClient client = new WebClient())
+        double stockPrice = GetStockPrice(jsonData);
+
+        //verifica se o valor encontrado é diferente do último valor verificado
+        if (stockPrice != currentQuote)
         {
-            // realizando requisicao para a url e salvando como string
-            string jsonData = client.DownloadString(queryUri);
-
-            try
+            // verifica se a cotação atingiu algum limite e envia o email
+            if (stockPrice >= upperBound)
             {
-                // obtendo o valor da ação a partir da chave"05. price"
-                JsonDocument jsonDocument = JsonDocument.Parse(jsonData);
-                JsonElement jsonElement = jsonDocument.RootElement.GetProperty("Global Quote");
-                
-                //obtem o valor 
-                stockPrice = GetStockPrice(jsonElement);
+                string body = "A ação " + stock + " atingiu R$" + stockPrice + ". venda!";
+                EmailSender.SendEmail(stock, body);
+
             }
-            catch (Exception e) 
+            else if (stockPrice <= lowerBound)
             {
-                Console.WriteLine(e.Message);   
-            }   
+                string body = "A ação " + stock + " atingiu R$" + stockPrice + ". compre!";
+                EmailSender.SendEmail(stock, body);
 
-            //verifica se o valor encontrado é diferente do último valor verificado
-            if (stockPrice != currentQuote)
-            {
-                // verifica se a cotação atingiu algum limite e envia o email
-                if (stockPrice >= upperBound)
-                {
-                    string body = "A ação " + stock + " atingiu R$" + stockPrice + ". venda!";
-                    EmailSender.SendEmail(stock, body);
-
-                }
-                else if (stockPrice <= lowerBound)
-                {
-                    string body = "A ação " + stock + " atingiu R$" + stockPrice + ". compre!";
-                    EmailSender.SendEmail(stock, body);
-
-                }
-
-                //salva o valor da cotação para controle
-                currentQuote = stockPrice;
             }
-            // se o valor se manteve, então não envia email.
-            else
-            {
-                return;
-            }
+
+            //salva o valor da cotação para controle
+            currentQuote = stockPrice;
         }
-    }
+        // se o valor se manteve, então não envia email.
+        else
+        {
+            return;
+        }
 
-    private static double GetStockPrice(JsonElement jsonElement)
+    }
+    private static double GetStockPrice(string jsonData)
     {
         try
         {
+            // obtendo o valor da ação a partir da chave"05. price"
+            JsonDocument jsonDocument = JsonDocument.Parse(jsonData);
+            JsonElement jsonElement = jsonDocument.RootElement.GetProperty("Global Quote");
             string stockPricestring = jsonElement.GetProperty("05. price").GetString();
             double price = Convert.ToDouble(stockPricestring, CultureInfo.InvariantCulture);
             return price;
